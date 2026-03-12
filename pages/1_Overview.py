32815ad9365f109b40c3d0d5d228c9b45373cf01
Theo <theo.busiris@epitech.eu>
2026-03-12 23:14:46 +0100

    add metrics

--- pages/1_Overview.py
+++ pages/1_Overview.py
@@ -1,13 +1,16 @@
 import streamlit as st
+from data_loader import aisles, order_product, orders, departments, products
 
 st.header('Project Overview')
 
-col1, col2, col3, col4 = st.columns(4)
+col1, col2, col3, col4, col5, col6 = st.columns(6)
 
-col1.metric('Total Orders', '3.4M')
-col2.metric('Total Users', '200K')
-col3.metric('Total Products', '49K')
-col4.metric('Departments', '21')
+col1.metric('Total Orders', f'{orders.shape[0]} 📈')
+col4.metric('Total ordered products', f'{order_product.shape[0]}📈')
+col2.metric('Total Users', f'{orders['user_id'].nunique()} 📈')
+col3.metric('Total Products', f'{products.shape[0]} 📉')
+col5.metric('Aisles', aisles.shape[0])
+col6.metric('Departments', departments.shape[0])
 
 st.markdown('---')
 
